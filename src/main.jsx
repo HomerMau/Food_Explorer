295a97bdd902009050e3fe8f0c6dc78ec17c4d63
Tiago Lucas Da Silva <titi020604@gmail.com>
2024-03-22 18:00:33 -0300

    feat: first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+
+import { Dish } from './pages/Dish';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Dish />
+  </React.StrictMode>
+)
